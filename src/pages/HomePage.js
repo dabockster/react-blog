25bf94b6acb92a9205b1c9cc7788bdf649d0021c
Steven Bock <steven@stevenbock.me>
2021-08-24 11:12:17 -0700

    Created first React component using React Router

---
+++ src/pages/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+        <h1>Hello, welcome to my blog!</h1>
+
+        <p>
+            Lorum ipsum blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        </p>
+        <p>
+            Lorum ipsum blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        </p>
+        <p>
+            Lorum ipsum blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        </p>
+    </>
+);
+
+export default HomePage;
