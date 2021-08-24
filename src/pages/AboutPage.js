50768d0bbc95cedee35d3a5c90818a39adcbf594
Steven Bock <steven@stevenbock.me>
2021-08-24 11:48:39 -0700

    Created blog pages using React Router

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <>
+        <h1>About Me</h1>
+
+        <p>
+            Lorum ipsum blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        </p>
+        <p>
+            Lorum ipsum blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        </p>
+        <p>
+            Lorum ipsum blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        </p>
+    </>
+);
+
+export default AboutPage;
---
+++ src/pages/ArticlesList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ArticlesList = () => (
+    <>
+        <h1>ArticlesList!</h1>
+    </>
+);
+
+export default ArticlesList;
