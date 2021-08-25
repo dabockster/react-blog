a21f169d8c2738a5d0782a75ba62de11102d44fb
Steven Bock <steven@stevenbock.me>
2021-08-24 17:55:34 -0700

    Created 404 page and trigger

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
 } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
@@ -8,6 +9,7 @@ import AboutPage from './pages/AboutPage';
 import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
 import NavBar from './NavBar';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
@@ -15,10 +17,15 @@ function App() {
       <div className="App">
         <NavBar />
         <div id="page-body">
-          <Route path ="/" component={HomePage} exact />
-          <Route path ="/about" component={AboutPage} />
-          <Route path ="/articles-list" component={ArticlesListPage} />
-          <Route path ="/article/:name" component={ArticlePage} />         
+          <Switch>
+            <Route path ="/" component={HomePage} exact />
+            <Route path ="/about" component={AboutPage} />
+            <Route path ="/articles-list" component={ArticlesListPage} />
+            <Route path ="/article/:name" component={ArticlePage} />
+            
+            {/* 404 pages always go last when using React Switch */}
+            <Route component={NotFoundPage} />
+          </Switch>
         </div>
       </div>
     </Router>
